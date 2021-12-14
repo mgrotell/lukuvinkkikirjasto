ac92b3001bd7fc2a2702268c2035d27e4982258b
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-12-14 10:22:54 +0200

    storage testin refaktorointi

--- src/test/java/storageTests/StorageTest.java
+++ src/test/java/storageTests/StorageTest.java
@@ -3,76 +3,82 @@ package storageTests;
 import lukuvinkkikirjasto.main.Tip;
 import lukuvinkkikirjasto.storage.Storage;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.File;
+
+import static org.junit.Assert.*;
 
 public class StorageTest {
 
 
     private Storage testStora;
+    static Tip tipNotUniqueHeader1;
+    static Tip tipNotUniqueHeader2;
+    static Tip tipUniqueHeader;
 
-    private int compareValueAddedTips = 0;
+    @BeforeClass
+    public static void initializeTips() {
+        tipNotUniqueHeader1 = new Tip("Help", "Us", "Hesus",
+                "heaven", "Book", "satanic", "meh", "muumi");
+        tipNotUniqueHeader2 = new Tip("Help", "test", "Jesus",
+                "https", "Blog", "", "joo", "muumuu");
+        tipUniqueHeader = new Tip("Best", "Us", "Hesus",
+                "heaven", "Book", "satanic", "meh", "muumi");
 
-
+    }
     @Before
     public void Initialize() {
         testStora = new Storage(true);
         testStora.deleteTestDatabase();
         testStora.initializeTestDatabase();
 
-
     }
 
+    @Test
+    public void deleteTestDatabaseRemovesTestDatabase() {
+        File testDatabaseFile = new File("test.db");
+        if (testDatabaseFile.exists()) {
+            testStora.initializeTestDatabase();
+            testDatabaseFile = new File("test.db");
+        }
+        assertTrue(testDatabaseFile.exists());
+        testStora.deleteTestDatabase();
+        testDatabaseFile = new File("test.db");
+        assertFalse(testDatabaseFile.exists());
+
+        testStora.initializeTestDatabase();
+        testDatabaseFile = new File("test.db");
+        assertTrue(testDatabaseFile.exists());
+    }
 
     @Test
     public void addOneToStorage() {
 
-        Tip tipOne = new Tip("Help", "Us", "Hesus",
-                "heaven", "Book", "satanic", "meh", "muumi");
-
-        compareValueAddedTips++;
-        testStora.addToStorage(tipOne);
+        testStora.addToStorage(tipNotUniqueHeader1);
 
         assertEquals(1, testStora.getStorage().size());
-
     }
 
 
     @Test
-    public void addTwoAlmostSameToStorage() {
+    public void addTwoTipsWithUniqueHeader() {
 
-        Tip tipOne = new Tip("Help", "Us", "Hesus",
-                "heaven", "Book", "satanic", "meh", "muumi");
-        compareValueAddedTips++;
-
-        Tip tipTwo = new Tip("Help", "Us", "Hesus",
-                "heaven", "Book", "satanic", "meh", "Narnia");
-        compareValueAddedTips++;
-
-        testStora.addToStorage(tipOne);
-        testStora.addToStorage(tipTwo);
-
-        assertEquals(compareValueAddedTips, testStora.getStorage().size());
-
+        testStora.addToStorage(tipNotUniqueHeader1);
+        testStora.addToStorage(tipUniqueHeader);
+        System.out.println(testStora.getStorage());
+        assertEquals(2, testStora.getStorage().size());
     }
 
 
     @Test
-    public void addTwoSameToStorage() {
+    public void cannotAddTwoTipsWithSameHeader() {
 
-        Tip tipOne = new Tip("Help", "Us", "Hesus",
-                "heaven", "Book", "satanic", "meh", "muumi");
-        compareValueAddedTips++;
-        Tip tipTwo = new Tip("Help", "Us", "Hesus",
-                "heaven", "Book", "satanic", "meh", "muumi");
-        compareValueAddedTips++;
-        testStora.addToStorage(tipOne);
+        testStora.addToStorage(tipNotUniqueHeader2);
+        testStora.addToStorage(tipNotUniqueHeader1);
 
-        testStora.addToStorage(tipTwo);
-
-        assertEquals(compareValueAddedTips, testStora.getStorage().size());
-
+        assertEquals(1, testStora.getStorage().size());
     }
 
 
